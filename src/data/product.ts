572535400ae90f77e8609b8c2a5c5cfe92af92e6
YahyaWebTales <yahya.farehan@webtales.fr>
2024-12-29 20:43:37 +0100

    fix: correct path for images

--- src/data/product.ts
+++ src/data/product.ts
@@ -1,59 +1,67 @@
+import mcproImg from '@/assets/img/mcpro.jpeg';
+import levonoImg from '@/assets/img/levono.png';
+import riderImg from '@/assets/img/rider.png';
+import ldlcImg from '@/assets/img/ldlc.png';
+import asusImg from '@/assets/img/asus.png';
+import rogImg from '@/assets/img/rog.png';
+import msiImg from '@/assets/img/msi.png';
+import padImg from '@/assets/img/pad.png';
+
 export default [
-    {
-      id: 1,
-      image: 'src/assets/img/mcpro.jpeg',
-      title: 'Macbook Pro',
-      description: 'Power and elegance.',
-      price: 1500,
-    },
-    {
-      id: 2,
-      image: 'src/assets/img/levono.PNG',
-      title: 'Levono Pro',
-      description: 'Performance for professionals.',
-      price: 2300,
-    },
-    {
-      id: 3,
-      image: 'src/assets/img/rider.PNG',
-      title: 'Rider',
-      description: 'Light and fast.',
-      price: 1200,
-    },
-    {
-      id: 4,
-      image: 'src/assets/img/ldlc.PNG',
-      title: 'LDLC Benolo',
-      description: 'Built to last.',
-      price: 4500,
-    },
-    {
-      id: 5,
-      image: 'src/assets/img/asus.PNG',
-      title: 'Asus Gamer',
-      description: 'Designed for gamers.',
-      price: 3755,
-    },
-    {
-      id: 6,
-      image: 'src/assets/img/rog.PNG',
-      title: 'Rog Desktop',
-      description: 'A battle station.',
-      price: 2452,
-    },
-    {
-      id: 7,
-      image: 'src/assets/img/msi.PNG',
-      title: 'MSI Play',
-      description: 'Powerful and versatile.',
-      price: 1478,
-    },
-    {
-      id: 8,
-      image: 'src/assets/img/pad.PNG',
-      title: 'ThinkPad',
-      description: 'Reliable everyday.',
-      price: 899,
-    },
-  ];
-  
+  {
+    id: 1,
+    image: mcproImg,
+    title: 'Macbook Pro',
+    description: 'Power and elegance.',
+    price: 1500,
+  },
+  {
+    id: 2,
+    image: levonoImg,
+    title: 'Levono Pro',
+    description: 'Performance for professionals.',
+    price: 2300,
+  },
+  {
+    id: 3,
+    image: riderImg,
+    title: 'Rider',
+    description: 'Light and fast.',
+    price: 1200,
+  },
+  {
+    id: 4,
+    image: ldlcImg,
+    title: 'LDLC Benolo',
+    description: 'Built to last.',
+    price: 4500,
+  },
+  {
+    id: 5,
+    image: asusImg,
+    title: 'Asus Gamer',
+    description: 'Designed for gamers.',
+    price: 3755,
+  },
+  {
+    id: 6,
+    image: rogImg,
+    title: 'Rog Desktop',
+    description: 'A battle station.',
+    price: 2452,
+  },
+  {
+    id: 7,
+    image: msiImg,
+    title: 'MSI Play',
+    description: 'Powerful and versatile.',
+    price: 1478,
+  },
+  {
+    id: 8,
+    image: padImg,
+    title: 'ThinkPad',
+    description: 'Reliable everyday.',
+    price: 899,
+  },
+];
